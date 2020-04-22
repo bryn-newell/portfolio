a07296bd4ff74caff7ffec6a31c9b15d3819e5bf
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2020-04-21 21:26:53 -0600

    adding linked in logo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="main" :style="{backgroundImage: `url(${bg})`}">
-    <h1 class="header">bryn newell -</h1>
+    <h1 class="header">bryn newell</h1>
     <p>description</p>
     <div class="social-links">
       <a href="https://github.com/bryn-newell" target="_blank">
         <img :src="github" alt="Github" class="logo">
       </a>
-      <a href="">
+      <a href="https://twitter.com/bryn_newell" target="_blank">
         <twitter class="logo" />
       </a>
-      <a href="">
+      <a href="https://medium.com/@bryn.newell" target="_blank">
         <medium class="logo" />
       </a>
+      <a href="https://www.linkedin.com/in/bryn-newell/" target="_blank">
+        <linkedin class="logo" />
+      </a>
     </div>
-    <ul>
+    <ul class="section-list">
       <li>Blog</li>
       <li>Projects</li>
       <li>Resume/CV</li>
@@ -26,12 +29,14 @@ import bg from '@/assets/mainBG.jpg';
 import github from '@/assets/github.png';
 import medium from '@/components/medium.vue';
 import twitter from '@/components/twitter.vue';
+import linkedin from '@/components/linkedin.vue';
 
 export default {
   name: 'Main',
   components: {
     medium,
     twitter,
+    linkedin,
   },
   data() {
     return {
@@ -44,8 +49,10 @@ export default {
 
 <style lang="scss">
   .main {
+    align-items: flex-end;
     background-size: cover;
     background-repeat: no-repeat;
+    display: flex;
     height: 100vh;
     width: 100vw;
 
@@ -60,6 +67,11 @@ export default {
 
     .logo {
       height: 30px;
+    }
+
+    .section-list {
+      font-size: 3rem;
+      list-style-type: none;
     }
   }
 </style>
