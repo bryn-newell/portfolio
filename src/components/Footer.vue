a22827b66ec56e3ad2456140a7be662f1d96209e
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2020-06-01 22:06:23 -0600

    Adding a basic footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
 	<div class="footer">
+		<p>copyright? bryn newell <span class="decoration" aria-hidden="true">—</span> 2020</p>
+		<p>design by <a href="https://henry.codes/">Henry Desroches</a></p>
 	</div>
 </template>
 
